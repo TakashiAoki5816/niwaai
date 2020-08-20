f5758813a6b7bb24b9cd021a2c4111bc4431e243
Takashi Aoki <arfaben_5816@icloud.com>
2020-08-20 18:40:22 +0900

    Change image null:true

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_072439) do
+ActiveRecord::Schema.define(version: 2020_08_20_093613) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
-    t.string "image", null: false
+    t.string "image"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
