ac79b39c5da676a955c158907d2b48cd5f091bfa
Takashi Aoki <arfaben_5816@icloud.com>
2020-08-25 22:43:15 +0900

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
-  resources :posts, only: [:new, :create, :show]
+  resources :posts, only: [:new, :create, :show, :edit, :update, :destroy]
   resources :users, only: :show
 end
