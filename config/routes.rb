a04ee1039bff91bf38d1cc3a08118b129273d924
Takashi Aoki <arfaben_5816@icloud.com>
2020-08-27 13:17:39 +0900

    added comment routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
-  resources :posts, only: [:new, :create, :show, :edit, :update, :destroy]
   resources :users, only: :show
+  resources :posts, only: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :comments, only: :create
+  end
 end
