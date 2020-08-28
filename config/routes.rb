83e4ea520814961229c8a655edae617edb9c61e4
Takashi Aoki <arfaben_5816@icloud.com>
2020-08-28 17:29:59 +0900

    added users-show likes-action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
-  resources :users, only: :show
+  resources :users, only: :show do
+    member do
+      get :likes
+    end
+  end
   resources :posts, only: [:new, :create, :show, :edit, :update, :destroy] do
     resources :comments, only: :create
     resources :likes, only: [:create, :destroy]
