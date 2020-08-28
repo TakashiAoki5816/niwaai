78c99332a2a9530ac124bd55a91096491340feec
Takashi Aoki <arfaben_5816@icloud.com>
2020-08-28 17:34:48 +0900

    added likes action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @post = current_user.posts
   end
+
+  def likes
+    @user = User.find(params[:id])
+    @like = @user.likes
+  end
 end
