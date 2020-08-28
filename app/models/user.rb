ce493656f9f639ad17d041bd2ec54e8e8e3fecfb
Takashi Aoki <arfaben_5816@icloud.com>
2020-08-28 15:19:22 +0900

    added like association

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,7 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :comments
+  has_many :likes, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post
   mount_uploader :image, AvaterUploader
 end
