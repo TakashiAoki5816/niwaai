6ae0d4b9ed2b7f48ba114e06e4c53d89f81ed303
Takashi Aoki <arfaben_5816@icloud.com>
2020-08-29 12:49:52 +0900

    added association relationship.rb user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,11 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
+
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
   mount_uploader :image, AvaterUploader
 
   def already_liked?(post)
