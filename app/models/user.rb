ef06901b4bbd3cce572bfb75cb38676b94f305cf
Takashi Aoki <arfaben_5816@icloud.com>
2020-08-30 23:51:26 +0900

    added follow-unfolow-following action

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,21 @@ class User < ApplicationRecord
   has_many :followers, through: :reverse_of_relationships, source: :user
   mount_uploader :image, AvaterUploader
 
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
+
   def already_liked?(post)
     self.likes.exists?(post_id: post.id)
   end
