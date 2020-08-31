f48759e719e1f3a29bfbeb1a63f84001b24ed0e5
Takashi Aoki <arfaben_5816@icloud.com>
2020-08-31 14:53:00 +0900

    added relationships route

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
     resources :comments, only: :create
     resources :likes, only: [:create, :destroy]
   end
+  resources :relationships, only: [:create, :destroy]
 end
