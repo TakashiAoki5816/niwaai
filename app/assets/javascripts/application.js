5c1f1cf96c8e33269e6365efacd322053215674d
Takashi Aoki <arfaben_5816@icloud.com>
2020-09-02 01:23:37 +0900

    Update post_params

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,5 +2,3 @@
 //= require popper
 //= require bootstrap
 //= require_tree .
-//= require underscore
-//= require gmaps/google
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -48,6 +48,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :garden_name, :image, :content).merge(user_id: current_user.id)
+    params.require(:post).permit(:title, :garden_name, :image, :content, :address, :latitude, :longitude).merge(user_id: current_user.id)
   end
 end
