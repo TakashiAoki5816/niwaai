eed1fd1de076d5b90b1e7dff28ea1ca09ef5cb71
Takashi Aoki <arfaben_5816@icloud.com>
2020-09-01 21:11:49 +0900

    added Post model address-geocode

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :liked_users, through: :likes, source: :user
   mount_uploader :image, ImageUploader
+  geocoded_by :address
+  after_validation :geocode
 
   validates :title, :garden_name, :image, presence: true
 end
