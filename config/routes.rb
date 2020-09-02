972d03d94a0ec5cfd96f04f1413f3767658109f2
Takashi Aoki <arfaben_5816@icloud.com>
2020-09-02 17:59:11 +0900

    added route guest-login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'home#index'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   resources :users, only: :show do
     member do
       get :likes
@@ -11,5 +15,6 @@ Rails.application.routes.draw do
     resources :likes, only: [:create, :destroy]
     resources :maps, only: [:index]
   end
+  root to: 'home#index'
   resources :relationships, only: [:create, :destroy]
 end
