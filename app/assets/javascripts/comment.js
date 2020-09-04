4fbb5e7866bfa9a56487bc6f4326cf083166722e
Takashi Aoki <arfaben_5816@icloud.com>
2020-09-04 17:23:29 +0900

    Update posts-show.html.haml

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,15 +1,40 @@
-$(function(){
-  $('#new_comment').on('submit', function(e){
-    e.preventDefault();
-    let formData = new FormData(this);
-    let url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-  })
-})
+// $(function(){
+//   function buildHTML(comment){
+//     let html = `<div class="Postsbox__Commentbox">
+//                   <div class="header">
+//                     <div class="header__left">
+//                       <div class="userimage">
+//                         <img src="${comment.user_image}" width="50" height="50">
+//                       </div>
+//                       <div class="right">
+//                         <div class="username">
+//                           ${comment.user_name}
+//                         </div>
+//                         <div class="created-at">
+//                           2020年09月02日05:16
+//                         </div>
+//                       </div>
+//                     </div>
+//                   </div>
+//                   <div class="Contents">
+//                     <div class="Contents__Comment">
+//                       あいうえお
+//                     </div>
+//                   </div>
+//                 </div>`
+//     return html;
+//   }
+//   $('#new_comment').on('submit', function(e){
+//     e.preventDefault();
+//     let formData = new FormData(this);
+//     let url = $(this).attr('action')
+//     $.ajax({
+//       url: url,
+//       type: "POST",
+//       data: formData,
+//       dataType: 'json',
+//       processData: false,
+//       contentType: false
+//     })
+//   })
+// })
