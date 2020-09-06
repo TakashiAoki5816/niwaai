9b327eecd6a0fd5bf8ce587a42d8a784fa6e5c01
Takashi Aoki <arfaben_5816@icloud.com>
2020-09-06 13:28:07 +0900

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,13 @@
 class LikesController < ApplicationController
   def create
-    @like = current_user.likes.create(post_id: params[:post_id])
-    redirect_to post_path(params[:post_id])
+    @post = Post.find(params[:post_id])
+    @like = current_user.likes.build(post_id: params[:post_id])
+    @like.save
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
     @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     @like.destroy
-    redirect_to post_path(params[:post_id])
   end
 end
