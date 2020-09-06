338c1ec340a54d37204647957405545c70846a20
Takashi Aoki <arfaben_5816@icloud.com>
2020-09-06 15:27:51 +0900

    added heart _like.html.haml

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,7 +7,7 @@ class LikesController < ApplicationController
 
   def destroy
     @post = Post.find(params[:post_id])
-    @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
-    @like.destroy
+    like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
+    like.destroy
   end
 end
