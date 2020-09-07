051d4d473b4e987160bc9b98a597c7e75169dfb8
Takashi Aoki <arfaben_5816@icloud.com>
2020-09-07 15:44:57 +0900

    Update _like.html.haml

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,7 @@
 class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @like = current_user.likes.build(post_id: params[:post_id])
-    @like.save
+    like = current_user.likes.create(post_id: params[:post_id])
   end
 
   def destroy
