b8d35df21f6f25ee1aad87739d97365a41d00aee
Takashi Aoki <arfaben_5816@icloud.com>
2020-09-17 16:43:38 +0900

    Edit

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     email                   {"test@example.com"}
     name                    {"tarou"}
-    image                   {"@@@.jpg"}
+    image                   {File.open("#{Rails.root}/public/images/test_image.jpg")}
     password                {"00000000"}
     password_confirmation   {"00000000"}
   end
