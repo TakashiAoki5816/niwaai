3150ba90f12a72d34f79ace359318fe3fb26f73f
Takashi Aoki <arfaben_5816@icloud.com>
2020-09-18 23:06:05 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,15 +3,19 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-    region: 'ap-northeast-1'
-  }
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  elsif Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region: 'us-east-2'
+    }
 
-  config.fog_directory  = 'niwaai2020'
-  config.asset_host = 'https://s3-us-east-2.amazonaws.com/niwaai2020'
+    config.fog_directory  = 'niwaai2020'
+    config.asset_host = 'https://s3-us-east-2.amazonaws.com/niwaai2020'
+  end
 end
