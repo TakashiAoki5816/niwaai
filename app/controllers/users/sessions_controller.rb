30f9f726ef15f90d9c38b03a04a019b1ff712e62
Takashi Aoki <arfaben_5816@icloud.com>
2020-09-28 15:26:44 +0900

    Changed redirect logout

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,4 +4,12 @@ class Users::SessionsController < Devise::SessionsController
     sign_in user
     redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
   end
+
+  def after_sign_in_path_for(resource)
+    root_path(resource)
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path(resource)
+  end
 end
