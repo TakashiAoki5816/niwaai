2b05e20db90dab2ce0ff5b3ba837cb2ba30b5ccd
Takashi Aoki <arfaben_5816@icloud.com>
2020-10-15 15:40:04 +0900

    Create user_sign_up_spec.rb

---
+++ spec/system/user_sign_up_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe 'Users', type: :system, js: true do
+  it 'ユーザーの新規登録ができる' do
+    visit '/users/sign_up'
+    fill_in 'user[name]',         with: 'tester'
+    fill_in 'user[email]',  with: 'tester@sample.com'
+    fill_in 'user[password]',    with: 'password'
+    fill_in 'user[password_confirmation]', with: 'password'
+    click_button '新規登録'
+    expect(page).to have_content "アカウント登録が完了しました。"
+    expect(current_path).to eq root_path
+  end
+end
