2a36fa799a95978692e1e9bf3c5ceb7cbaf13d1e
Takashi Aoki <arfaben_5816@icloud.com>
2020-10-16 17:14:57 +0900

    Changed Users order DESC

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all.page(params[:page]).per(8)
+    @users = User.all.order(created_at: "DESC").page(params[:page]).per(8)
   end
 
   def show
