0e395cf2d36a07d43b7ba558638a6a831faf6284
Takashi Aoki <arfaben_5816@icloud.com>
2020-10-17 23:06:37 +0900

    Create user_sign_spec.rb

--- spec/system/user_sign_up_spec.rb
+++ spec/system/user_sign_up_spec.rb
@@ -1,14 +1,26 @@
 require "rails_helper"
 
 RSpec.describe 'Users', type: :system, js: true do
-  it 'ユーザーの新規登録ができる' do
-    visit '/users/sign_up'
-    fill_in 'user[name]',         with: 'tester'
-    fill_in 'user[email]',  with: 'tester@sample.com'
-    fill_in 'user[password]',    with: 'password'
-    fill_in 'user[password_confirmation]', with: 'password'
-    click_button '新規登録'
-    expect(page).to have_content "アカウント登録が完了しました。"
-    expect(current_path).to eq root_path
+  context '新規ユーザーの場合' do
+    before do
+      visit '/users/sign_up'
+      fill_in 'user[name]',         with: 'tester'
+      fill_in 'user[email]',  with: 'tester@sample.com'
+      fill_in 'user[password]',    with: 'password'
+      fill_in 'user[password_confirmation]', with: 'password'
+    end
+
+    it 'ユーザーの新規登録ができる' do
+      click_button '新規登録'
+      expect(page).to have_content "アカウント登録が完了しました。"
+      expect(current_path).to eq root_path
+    end
+
+    it "ログインをクリックした場合ログインページに遷移する" do
+      find('.Login-Button').click
+      expect(current_path).to eq new_user_session_path
+    end
+
+
   end
 end
